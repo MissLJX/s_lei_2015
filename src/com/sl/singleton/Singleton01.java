ad3540592d5d5d331add8e4490ed933c3bd9ff49
MissLJX <shaoleitanglili@163.com>
2015-04-13 20:56:39 +0800

    add comments on code
    add comments on code

--- src/com/sl/singleton/Singleton01.java
+++ src/com/sl/singleton/Singleton01.java
@@ -13,6 +13,8 @@ public class Singleton01 {
 	private static Singleton01 instance = null;
 
 	public static Singleton01 getInstance() {
+		//判断instance是否为空
+		//当instance为空时实例化一个对象，确保系统有且只有一个该类的实例
 		if (instance == null) {
 			instance = new Singleton01();
 		}
